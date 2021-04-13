0b5b36f497bc331c9bc8c1f23f931ae7f5869422
Indra Gunawan <guind.online@gmail.com>
2021-04-14 00:48:26 +0700

    remove phpunit, use ProphecyTrait

--- Tests/EventListener/HeaderModificationListenerTest.php
+++ Tests/EventListener/HeaderModificationListenerTest.php
@@ -13,6 +13,7 @@ namespace Indragunawan\ApiRateLimitBundle\Tests\EventListener;
 
 use Indragunawan\ApiRateLimitBundle\EventListener\HeaderModificationListener;
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,6 +23,8 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class HeaderModificationListenerTest extends TestCase
 {
+    use ProphecyTrait;
+
     private $kernel;
 
     protected function setUp(): void
